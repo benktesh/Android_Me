7b8379cbbb6da492cb16a824e82e56cd88273f51
Benktesh Sharma <benktesh@yahoo.com>
2018-06-14 06:31:30 -0500

    Completes 04 Fragment

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+
+import com.example.android.android_me.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
